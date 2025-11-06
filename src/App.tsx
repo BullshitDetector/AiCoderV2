c9972f5a8b31e9bb003ff3eb7480a99e17307f14
R3almAIDeveloper <r3almdev@r3alm.com>
2025-11-06 11:11:08 -0500

    V3

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,12 @@ import { Layout } from "./components/Layout";
 export default function App() {
   useEffect(() => {
     const handler = (e: MessageEvent) => {
-      if (e.data.type === 'serverReady' && e.data.url) {
-        window.postMessage(e.data, '*');
+      if (e.data.type === "serverReady" && e.data.url) {
+        window.postMessage(e.data, "*");
       }
     };
-    window.addEventListener('message', handler);
-    return () => window.removeEventListener('message', handler);
+    window.addEventListener("message", handler);
+    return () => window.removeEventListener("message", handler);
   }, []);
 
   return <Layout />;
